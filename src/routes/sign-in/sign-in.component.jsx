513eb0a516836919db44331ecdc03d70e5e35187
User <user@windows.com>
2023-02-07 18:36:26 +0530

    sign up component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,15 +3,20 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign In</h1>
-      <button onClick={logGoogleUser}>Sign In with Google Account</button>
+      <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+
+      <SignUpForm />
     </div>
   );
 };
